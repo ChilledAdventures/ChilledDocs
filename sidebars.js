fb4d0cbd684d2d2b91ca5bec51823f48a61d3c7d
Fantom250YT <73852683+Fantom250YT@users.noreply.github.com>
2022-01-05 19:00:31 +0000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,13 +1,4 @@
-/**
- * Creating a sidebar enables you to:
- - create an ordered group of docs
- - render a sidebar for each doc of that group
- - provide next/previous navigation
-
- The sidebars can be generated from the filesystem, or explicitly defined here.
-
- Create as many sidebars as you want.
- */
+// Sidebars are defined here - Automatic sidebars is turned off!
 
 // @ts-check
 
