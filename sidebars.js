3b30b08b0048627e76d6e8bfa8d7ac25c0620122
Fantom250YT <73852683+Fantom250YT@users.noreply.github.com>
2022-02-16 17:34:41 +0000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -30,15 +30,25 @@ const sidebars = {
       type: 'category',
       label: 'Kingdoms',
       items: [
-          'kingdoms/getting_started/commands',
-          'kingdoms/getting_started/online_map',
-          'kingdoms/getting_started/structures',
-          'kingdoms/getting_started/turrets',
-          'kingdoms/getting_started/upgrades',
-          'kingdoms/enchants/e-general-info',
-          'kingdoms/enchants/enchant_list',
-      ]
-},
+{
+      type: 'category',
+      label: "Getting Started",
+      items: [
+          'getting_started/commands',
+          'getting_started/online_map',
+          'getting_started/structures',
+          'getting_started/turrets',
+          'getting_started/upgrades',
+                    ]
+                },
+{
+      type: 'category',
+      label: "Enchantments",
+      items: [
+          'enchants/e_general_info',
+          'enchants/enchantment_list',
+                    ]
+                },
 {
       type: 'category',
       label: 'Services',
@@ -49,6 +59,8 @@ const sidebars = {
           'services/cancel-service',
           'services/server-specs',
       ],
+    },
+  ],
 },
   ],
 };
